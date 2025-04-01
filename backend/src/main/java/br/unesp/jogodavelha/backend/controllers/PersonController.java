e0a76cf5747bcf43542d962bd938d946c1d176ac
EduardoMRT <eduksoficial@gmail.com>
2025-04-01 15:08:15 -0300

    + chamadas rest p Pessoa

--- backend/src/main/java/br/unesp/jogodavelha/backend/controllers/PersonController.java
+++ backend/src/main/java/br/unesp/jogodavelha/backend/controllers/PersonController.java
@@ -1,12 +1,12 @@
 package br.unesp.jogodavelha.backend.controllers;
 
 import br.unesp.jogodavelha.backend.domain.Person;
+import br.unesp.jogodavelha.backend.exceptions.PersonException;
 import br.unesp.jogodavelha.backend.service.PersonService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/person")
@@ -21,6 +21,33 @@ public class PersonController {
     @GetMapping("/list")
     public List<Person> list(){
         return personService.list();
+    }
+
+    @PostMapping("/create/{name}/{mail}/{age}/{country}")
+    public String create(@PathVariable("name") String name, @PathVariable("mail") String mail,
+                         @PathVariable("age") Integer age, @PathVariable("country") String country){
+        this.personService.createOrUpdate(new Person(name, mail, age, country));
+        return "Person created successfully";
+    }
+
+    @GetMapping("/searchByMail/{mail}")
+    public Person searchByMail(@PathVariable("mail") String mail){
+        return this.personService.findPersonByMail(mail);
+    }
+
+    @GetMapping("/findById/{id}")
+    public Person findById(@PathVariable("id") Integer id){
+        Optional<Person> person = this.personService.find(new Person(id));
+        if(person.isPresent()){
+            return person.get();
+        }else{
+            throw new PersonException("Person not found");
+        }
+    }
+
+    @DeleteMapping("/delete/{mail}")
+    public Boolean deleteByMail(@PathVariable("mail") String mail){
+        return this.personService.delete(personService.findPersonByMail(mail));
     }
 
 }
