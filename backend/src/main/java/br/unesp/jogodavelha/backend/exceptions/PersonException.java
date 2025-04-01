61b45665ee48a3a73fa1f66e5ceb3438ade1cfab
EduardoMRT <eduksoficial@gmail.com>
2025-04-01 11:41:34 -0300

    Exception e FindByMail

---
+++ backend/src/main/java/br/unesp/jogodavelha/backend/exceptions/PersonException.java
@@ -0,0 +1,9 @@
+package br.unesp.jogodavelha.backend.exceptions;
+
+public class PersonException extends RuntimeException {
+
+    public PersonException(String message){
+        super(message);
+    }
+
+}
--- backend/src/main/java/br/unesp/jogodavelha/backend/repository/PersonRespository.java
+++ backend/src/main/java/br/unesp/jogodavelha/backend/repository/PersonRespository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PersonRespository extends JpaRepository<Person, Long> {
+
+    Person findByMail(String mail);
+
 }
